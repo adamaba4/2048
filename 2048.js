61942243485104f9b9b708a642332cb39f2dbe85
Adama Ba <adama.ba@edu.univ-paris13.fr>
2026-03-03 17:37:57 +0100

    placement de l'entier 2 dans 2 cases vides aléatoirement de la grille

--- 2048.js
+++ 2048.js
@@ -1,63 +1,96 @@
 $(document).ready(() => {
     let grille = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    let score = 12
+    let score = 0
     let nbVide = 16
-
     function construitGrille() {
-        $("div#grille").append(
-            `<table
-             <tbody>
-              <tr>
-                <td>${grille[0][0]}</td> 
-                <td>${grille[0][1]}</td> 
-                <td>${grille[0][2]}</td> 
-                <td>${grille[0][3]}</td>
-                </tr>
-              <tr>
-                <td>${grille[1][0]}</td>
-                <td>${grille[1][1]}</td>
-                <td>${grille[1][2]}</td>
-                <td>${grille[1][3]}</td>
-              </tr>
-              <tr>
-                <td>${grille[2][0]}</td>
-                <td>${grille[2][1]}</td>
-                <td>${grille[2][2]}</td>
-                <td>${grille[2][3]}</td>
-              </tr>
-              <tr>
-                <td>${grille[3][0]}</td>
-                <td>${grille[3][1]}</td>
-                <td>${grille[3][2]}</td>
-                <td>${grille[3][3]}</td>
-              </tr>
-            </tbody>
-            </table>`
-        )
+        const $table = $("<table>")
+        const $tbody = $("<tbody>")
 
 
+        for (let i = 0; i < 4; i++)
+        {
+            const $tr = $("<tr>")
+            for (let j = 0; j < 4; j++)
+            {
+                const val = grille[i][j]
+
+                const $td = $("<td>").text(val !== 0 ? val : "")
+                $tr.append($td)
+            }
+            $tbody.append($tr)
+
+        }
+        $table.append($tbody)
+        $("#grille").append($table)
     }
+
+
     function afficheScore() {
-        $("span#score").replaceWith(score)
+        $("span#score").text(score)
 
     }
     function caseVide(k, x) {
         let compteur = 0
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
-                compteur++
-                if (compteur === k && grille[i][j] === 0) {
-                    grille[i][j] = x
-                    nbVide--
+                if ( grille[i][j] === 0) {
+                    if (compteur === k )
+                    {
+                        grille[i][j] = x
+                        nbVide--
+                        return
+                    }
+                    compteur++
+                }
+
+            }
+        }
+
+    }
+    function nouvelle() {
+        score = 0
+        afficheScore()
+        $("#grille").empty()
+        for (let i = 0; i < 4; i++)
+        {
+            for (let j = 0; j < 4; j++ )
+            {
+                grille[i][j] = 0
+            }
+        }
+        nbVide = 16
+
+        let c1 = Math.floor(Math.random() * nbVide)
+        caseVide(c1, 2)
+        let c2 = Math.floor(Math.random() * nbVide)
+        caseVide(c2, 2)
+
+        construitGrille()
+
+    }
+
+    function gameOver() {
+
+
+    }
+    function changerCoulerCase() {
+        for(let i= 0; i < 4; i++)
+        {
+            for (let j = 0; j < 4; j++)
+            {
+                if (grille[i][j] === 2)
+                {
+
                 }
             }
         }
 
-
-        construitGrille()
     }
-    caseVide(5, 7)
-    afficheScore()
 
+    $(function (){
+        nouvelle()
+    })
+    $("div button").click(nouvelle)
 
 })
+
